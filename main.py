76ebfc6ce7f3b8fd340e4f17ed4b483edf1db253
bmoran4 <62190152+bmoran4@users.noreply.github.com>
2020-03-14 18:24:00 -0400

    Create main.py
    Initial code

---
+++ main.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+import collections
+import sys
+
+# Functions
+
+def false():
+	print('false')
+	sys.exit(0)
+
+def check_string1(string):
+    characterCount = collections.defaultdict(int)
+
+    for character in string:
+        if character in characterCount.keys():
+            false()
+        else:
+            characterCount[character] = 1
+
+def check_length(str1, str2):
+    if len(str1) > len(str2):
+        false()
+    else:
+        pass
+
+
+def check_mapping(str1, str2):
+    mapping = {}
+    element = int(0)
+    
+    for character in str1:
+        if character in mapping.keys():
+            if str2[element] == mapping[character]:
+                pass
+            else:
+                false()
+        else:
+            mapping[character] = str2[element]
+
+        element += 1
+
+
+def main():
+    string1   = sys.argv[1]
+    string2   = sys.argv[2]
+
+    check_string1(string1)
+    check_length(string1, string2)
+    check_mapping(string1, string2)
+
+    print('true')
+    
+# Main Execution
+
+if __name__ == '__main__':
+    main()
+# vim: set sts=4 sw=4 ts=8 expandtab ft=python:
