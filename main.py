d912f4d123cf17d0873ef0de42ba809bfcb376b2
bmoran4 <62190152+bmoran4@users.noreply.github.com>
2020-03-19 15:57:14 -0400

    Updated main.py
    Added an error if input is incorrect for the program and deleted unnecessary lines of code

--- main.py
+++ main.py
@@ -12,9 +12,6 @@ def false():
 def check_length(str1, str2):
     if len(str1) > len(str2):
         false()
-    else:
-        pass
-
 
 def check_mapping(str1, str2):
     mapping = {}
@@ -22,27 +19,29 @@ def check_mapping(str1, str2):
     
     for character in str1:
         if character in mapping.keys():
-            if str2[element] == mapping[character]:
-                pass
-            else:
+            if str2[element] != mapping[character]:
                 false()
-        else:
-            mapping[character] = str2[element]
-
-        element += 1
-
+    else:
+        mapping[character] = str2[element]
+    element += 1
 
 def main():
-    string1   = sys.argv[1]
-    string2   = sys.argv[2]
+    
+    #Check Input
+    if len(sys.argv) != 3:
+        print("Error: Wrong Number of Inputs)
+        sys.exit(1)
+        
+	string1   = sys.argv[1]
+	string2   = sys.argv[2]
+    
+	check_length(string1, string2)
+	check_mapping(string1, string2)
 
-    check_length(string1, string2)
-    check_mapping(string1, string2)
-
-    #All tests passed
-    print('true')
+	#All tests passed
+	print('true')
     
 # Main Execution
 
 if __name__ == '__main__':
-    main()
+	main()
